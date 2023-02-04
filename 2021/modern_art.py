da0432f5c8a90dffcbc8d602691bb48ff2e7865f
Arnav Shah <shaharnav03@gmail.com>
2023-02-04 12:30:05 -0500

    bugfix

--- 2021/modern_art.py
+++ 2021/modern_art.py
@@ -1,22 +1,20 @@
 m = int(input())
 n = int(input())
 k = int(input())
-grid = [[1 for _ in n] for __ in m]
+grid = [[1 for _ in range(n)] for __ in range(m)]
+grid_copy = grid
 for i in range(k):
     command = input().split()
     command[1] = int(command[1]) - 1
-    if command == "R":
+    if command[0] == "R":
         for i in range(n):
             grid[command[1]][i] *= -1
     else:
-        assert command == "C"
+        assert command[0] == "C"
         for i in range(m):
             grid[i][command[1]] *= -1
 
-gold = 0
-for i in m:
-    for j in n:
-        if j == -1:
-            gold += 1
-
-print(gold)
+total = 0
+for arr in grid:
+    total += sum(arr)
+print(int((m * n - total) / 2))
