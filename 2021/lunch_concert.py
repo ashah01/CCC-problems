ac4e52435f183e633118c8308973c59bbbb7ad1f
Arnav Shah <shaharnav03@gmail.com>
2023-02-06 21:08:51 -0500

    nonsmooth optimization won't work, use iteration

---
+++ 2021/lunch_concert.py
@@ -0,0 +1,22 @@
+n = int(input())
+positions = []
+speeds = []
+hearing_ranges = []
+for i in range(n):
+    friend_data = list(map(int, input().split()))
+    positions.append(friend_data[0])
+    speeds.append(friend_data[1])
+    hearing_ranges.append(friend_data[2])
+
+total_seconds = float("inf")
+best_c = positions[0]
+for c in range(min(positions), max(positions)):
+    tmp = 0
+    for i in range(n):
+        tmp += max([0, (abs(c - positions[i]) - hearing_ranges[i])]) * speeds[i]
+    if tmp < total_seconds:
+        total_seconds = tmp
+        best_c = c
+
+print(best_c)
+print(total_seconds)
