e67033141c4277d6c9e5f608efd03e682b4e3db9
Arnav Shah <shaharnav03@gmail.com>
2023-02-14 17:45:18 -0500

    Working on test cases and even

--- 2022/good_triplets.py
+++ 2022/good_triplets.py
@@ -1,8 +1,8 @@
-# n, c = list(map(int, input().split()))
+n, c = list(map(int, input().split()))
 
-# points = list(map(int, input().split())) # key = index, value = value
-n, c = 8, 10
-points = [0, 2, 5, 5, 6, 9, 0, 0]
+points = list(map(int, input().split())) # key = index, value = value
+# n, c = 8, 10
+# points = [0, 2, 5, 5, 6, 9, 0, 0]
 
 def is_good_triplet(a, b, c_):
     # Test if c/2 works on odd circumferences
@@ -18,17 +18,27 @@ def is_good_triplet(a, b, c_):
     if (c_ == period_addition(b, c//2)):
         return False
     
-    x: int
+    x = a + c//2
     y: int
-    if period_addition(a, c//2) == 0:
-        x = c
-    else:
-        x = period_addition(a, c//2)
+    bs = []
     
-    if period_addition(b, c//2) == 0:
-        y = c
+    if (b > c // 2):
+        
+        if (c_ == 0):
+            c_ = c
+
+        bs.append(b + c//2)
+        bs.append(b - c // 2)
+    
+        if (abs(x - bs[0]) < abs(x - bs[1])):
+            y = bs[0]
+        
+        else:
+            y = bs[1]
+    
     else:
-        y = period_addition(b, c//2)
+        y = b + c//2
+        
 
     if (c_ in range(min(x, y), max(x, y))):
         return True
@@ -47,8 +57,25 @@ for i in range(n-2):
     for j in range(i+1, n-1):
         for k in range(j+1, n):
             if is_good_triplet(points[i], points[j], points[k]):
-                print(i + 1, j + 1, k + 1)
-            #     total_triplets += 1
+                total_triplets += 1
+
+print(total_triplets)
 
 
 # TODO: determine why some of the triplets aren't showing and why some excess are.
+"""
+
+Working: GOOD
+(1, 2, 5)
+(2, 3, 6)
+(2, 4, 5)
+(2, 5, 6)
+
+Not working: GOOD
+(2, 5, 7)
+(2, 5, 8)
+
+Excess: GOOD
+(1, 5, 6)
+
+"""
